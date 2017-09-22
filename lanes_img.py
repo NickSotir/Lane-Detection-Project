70728fd6282a830f1ed0d65e6f66d58cef6eebd9
NickSotir <nik.sotir@outlook.com>
2017-09-22 15:15:16 +0300

    Added Code

--- lanes_img.py
+++ lanes_img.py
@@ -2,7 +2,6 @@ import cv2
 import numpy as np
 #import matplotlib.py
 
-
 def filter_region(image, vertices):
     """
     Create the mask using the vertices and apply it to the input image
@@ -28,23 +27,28 @@ def select_region(image):
     vertices = np.array([[bottom_left, top_left, top_right, bottom_right]], dtype=np.int32)
     return filter_region(image, vertices)
 
+def Gaussian_Blur(image, kernel_size):
+    return cv2.GaussianBlur(image,(kernel_size,kernel_size),0)
+
+def Hough_line(image, minLength, maxGap, thresh):
+    return cv2.HoughLinesP(image, rho = 1, theta = np.pi/180, threshold = thresh, minLineLength = minLength, maxLineGap = maxGap)
 
 img = cv2.imread('solidWhiteRight.jpg')
 
 lower_white = np.uint8([0,200,0])
 upper_white = np.uint8([255,255,255])
 
-white = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+white = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
 
 white_mask = cv2.inRange(white, lower_white,upper_white)
 masked = cv2.bitwise_and(img, img, mask = white_mask)   #Filter out all colors except for white
 
 gray = cv2.cvtColor(masked, cv2.COLOR_BGR2GRAY)
-blur = cv2.GaussianBlur(gray,(13,13),0)
+blur = Gaussian_Blur(gray, 15)
 edges = cv2.Canny(blur, 50,150)
 edges = select_region(edges)
 
-lines = cv2.HoughLinesP(edges,rho = 1,theta = np.pi/180,threshold = 20,minLineLength = 20,maxLineGap = 300)
+lines = Hough_line(edges, 20,300,20)
 
 for line in lines:
     for x1,y1,x2,y2 in line:
